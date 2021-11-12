0d2b33e4678f6de3c424799fdc4221098a952c06
Leandro Lucas <LeoViolinist22@gmail.com>
2021-11-12 20:29:46 -0300

    adicionando a marllon do codigo do colete

--- Codigo_colete/Codigo_colete.ino
+++ Codigo_colete/Codigo_colete.ino
@@ -1,19 +1,72 @@
+
+
+/* begin Marllon  */ 
+#include <Adafruit_NeoPixel.h> 
+ #ifdef __AVR__  
+  #include <avr/power.h>  
+ #endif  
+   
+ 
+ int LED = 6;
+ 
+ #define NUMPIXELS   15
+   
+   
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LED, NEO_GRB + NEO_KHZ800); 
+/* End marlon */
+
+/* begin Renata  */
 #define SETA_ESQUERDA 13
 #define SETA_DIREITA 7
 
+/* End Renata  */
+
+
+
+/*  Begin Renata  */
 String mensagem;
 unsigned long int tempoAtual;
 unsigned long int tempoAuxEsquerdo;
 unsigned long int tempoAuxDireito;
 bool estadoDaSetaEsquerda = 0;
 bool estadoDaSetaDireita = 0;
+/* End Renata */
+
+/*  Begin Marllon  */
+char B;
+ 
+ int a = 0; 
+ int b = 1;  
+ int c = 2;
+ int d = 3;
+ int e = 4;
+ int f = 5;
+ int g = 6;
+ int h = 7;
+ int i = 8;
+ int j = 9;
+ int k = 10;
+ int l = 11;
+ int m = 12; 
+ int n = 13;
+ int o = 14;
+/*  End Marllon */
 
 void setup(){
+  /*  Begin Renata  */
   pinMode(SETA_ESQUERDA, OUTPUT);
   pinMode(SETA_DIREITA, OUTPUT);
   Serial.begin(9600);
+  /*  End Renata  */
+
+  /*  Begin Marllon */
+  pinMode(LED, OUTPUT);
+  pixels.begin();
+  /*  End Marllon   */
+  
 }
 
+/*  Begin Renata  */
 void SetaDireita(unsigned long int tempo, unsigned long int *comparacao){
   if(tempo - *comparacao == 200){
       digitalWrite(SETA_DIREITA, !digitalRead(SETA_DIREITA));
@@ -27,6 +80,7 @@ void SetaEsquerda(unsigned long int tempo, unsigned long int *comparacao){
     *comparacao = millis();
   }
 }
+/*  End Renata  */
 
 void loop(){
 
@@ -55,8 +109,118 @@ void loop(){
     SetaDireita(tempoAtual, &tempoAuxDireito);
   }else{
     digitalWrite(SETA_DIREITA, LOW);
+    
   }
+
   /*  End Renata  */
+
+  /* Begin Marllon  */
+    switch (mensagem)
+{
+ case 'A':
+   pixels.setPixelColor(a, pixels.Color(255,0,0));
+   pixels.show();   
+   
+   pixels.setPixelColor(b, pixels.Color(255,0,0));
+   pixels.show();   
+   
+   pixels.setPixelColor(c, pixels.Color(255,0,0));
+   pixels.show();   
+   
+   pixels.setPixelColor(d, pixels.Color(255,0,0));
+   pixels.show();   
+   
+   pixels.setPixelColor(e, pixels.Color(255,0,0));
+   pixels.show();   
+   
+   pixels.setPixelColor(f, pixels.Color(255,0,0));
+   pixels.show(); 
+
+   pixels.setPixelColor(g, pixels.Color(255,0,0));
+   pixels.show(); 
+
+   pixels.setPixelColor(h, pixels.Color(255,0,0));
+   pixels.show(); 
+
+   pixels.setPixelColor(i, pixels.Color(255,0,0));
+   pixels.show(); 
+   
+   pixels.setPixelColor(j, pixels.Color(255,0,0));
+   pixels.show(); 
+
+   pixels.setPixelColor(k, pixels.Color(255,0,0));
+   pixels.show(); 
+
+   pixels.setPixelColor(l, pixels.Color(255,0,0));
+   pixels.show(); 
+
+   pixels.setPixelColor(m, pixels.Color(255,0,0));
+   pixels.show();
+
+   pixels.setPixelColor(n, pixels.Color(255,0,0));
+   pixels.show();
+
+   pixels.setPixelColor(o, pixels.Color(255,0,0));
+   pixels.show();
+
+   break;
+
+   case 'a':
+      
+   pixels.setPixelColor(a, pixels.Color(0,0,0));
+   pixels.show();   
+   
+   pixels.setPixelColor(b, pixels.Color(0,0,0));
+   pixels.show();   
+  
+   pixels.setPixelColor(c, pixels.Color(0,0,0));
+   pixels.show();   
+   
+   pixels.setPixelColor(d, pixels.Color(0,0,0));
+   pixels.show();   
+  
+   pixels.setPixelColor(e, pixels.Color(0,0,0));
+   pixels.show();   
+  
+   pixels.setPixelColor(f, pixels.Color(0,0,0));
+   pixels.show();
+
+   pixels.setPixelColor(g, pixels.Color(0,0,0));
+   pixels.show();
+
+   pixels.setPixelColor(h, pixels.Color(0,0,0));
+   pixels.show();
+
+   pixels.setPixelColor(i, pixels.Color(0,0,0));
+   pixels.show();
+
+   pixels.setPixelColor(j, pixels.Color(0,0,0));
+   pixels.show();
+
+   pixels.setPixelColor(k, pixels.Color(0,0,0));
+   pixels.show();
+
+   pixels.setPixelColor(l, pixels.Color(0,0,0));
+   pixels.show();
+
+   pixels.setPixelColor(m, pixels.Color(0,0,0));
+   pixels.show();
+
+   pixels.setPixelColor(n, pixels.Color(0,0,0));
+   pixels.show();
+
+   pixels.setPixelColor(o, pixels.Color(0,0,0));
+   pixels.show();
+   
+   break;
+}
+
+  
+  /*  End Marllon */
+ 
+
+  
+ 
 
   
 
