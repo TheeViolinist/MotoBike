a915cbb49dbce8079a17fd7d3a95daa0a0ff0e6b
Leandro Lucas <LeoViolinist22@gmail.com>
2021-11-12 20:02:04 -0300

    ajustando o freios_setas

--- Freios_setas/Freios_setas.ino
+++ Freios_setas/Freios_setas.ino
@@ -1,10 +1,9 @@
-#define SETA_ESQUERDA 13
-#define SETA_DIREITA 7
-
+#define BOTAO_ESQUERDO 13
+#define BOTAO_DIREITO 7
 #define LED 2 //Definição da porta em que está conectado o pino do primeiro Led
 #define BLED 5//Definiçaõ da porta em que está conectado o pino do Segundo Led
 #define tLED 6//Definiçaõ da porta em que está conectado o pino do terceiro Led
-#define qLED 7//Definiçaõ da porta em que está conectado o pino do quarto Led
+#define qLED 9//Definiçaõ da porta em que está conectado o pino do quarto Led
 #define cLED 8//Definiçaõ da porta em que está conectado o pino do quinto Led
 #define BOTAOA 3 // Definição da porta em que está conectado o botão do freio dianteiro
 #define BOTAOB 4 // Definição da porta em que está conectado o botão do freio traseiro
@@ -12,16 +11,16 @@
 int estadoA; // Variáveis globais para os botões
 int estadoB;
 
-String mensagem;
-unsigned long int tempoAtual;
-unsigned long int tempoAuxEsquerdo;
-unsigned long int tempoAuxDireito;
-bool estadoDaSetaEsquerda = 0;
-bool estadoDaSetaDireita = 0;
+unsigned long tempoAtual;
+unsigned long tempoAuxiliar;
+int estadoDoBotaoEsquerdo;
+int estadoDoBotaoDireito;
+int auxiliar = 1;
 
 void setup(){
-  pinMode(SETA_ESQUERDA, OUTPUT);
-  pinMode(SETA_DIREITA, OUTPUT);
+  Serial.begin(9600);
+  pinMode(BOTAO_ESQUERDO, INPUT);
+  pinMode(BOTAO_DIREITO, INPUT);
   pinMode(LED, OUTPUT); // Modo de leitura do led, Output = saída
   pinMode(BLED, OUTPUT); // Modo de leitura do led, Output = saída
   pinMode(tLED, OUTPUT); // Modo de leitura do led, Output = saída
@@ -29,56 +28,32 @@ void setup(){
   pinMode(cLED, OUTPUT); // Modo de leitura do led, Output = saída
   pinMode(BOTAOA, INPUT); // Modo de leitura do botão A, Input = entrada
   pinMode(BOTAOB, INPUT); // Modo de leitura do botão B, Input = entrada
-  Serial.begin(9600);
-}
-
-void SetaDireita(unsigned long int tempo, unsigned long int *comparacao){
-  if(tempo - *comparacao == 200){
-      digitalWrite(SETA_DIREITA, !digitalRead(SETA_DIREITA));
-    *comparacao = millis();
-  }
-}
-
-void SetaEsquerda(unsigned long int tempo, unsigned long int *comparacao){
-  if(tempo - *comparacao == 200){
-      digitalWrite(SETA_ESQUERDA, !digitalRead(SETA_ESQUERDA));
-    *comparacao = millis();
-  }
 }
 
 void loop(){
   tempoAtual = millis();
+  estadoDoBotaoEsquerdo = digitalRead(BOTAO_ESQUERDO);
+  estadoDoBotaoDireito = digitalRead(BOTAO_DIREITO);
   
-  if(Serial.available() > 0){
-    mensagem = Serial.readStringUntil('\n');
-    
-    if(mensagem == "Seta Esquerda"){
-      estadoDaSetaEsquerda = !estadoDaSetaEsquerda;
-      tempoAuxEsquerdo = tempoAtual;
-    }
-    if(mensagem == "Seta Direita"){
-      estadoDaSetaDireita = !estadoDaSetaDireita;
-      tempoAuxDireito = tempoAtual;
-    }
+  if(tempoAtual-2000 == tempoAuxiliar){
+    auxiliar = 1;
   }
-  
-  if(estadoDaSetaEsquerda){
-    SetaEsquerda(tempoAtual, &tempoAuxEsquerdo);
-  }else{
-    digitalWrite(SETA_ESQUERDA, LOW);
+  if(estadoDoBotaoEsquerdo && auxiliar){
+    Serial.write("Seta Esquerda\n");
+    tempoAuxiliar = tempoAtual;
+    auxiliar = 0;
   }
-  if(estadoDaSetaDireita){
-    SetaDireita(tempoAtual, &tempoAuxDireito);
-  }else{
-    digitalWrite(SETA_DIREITA, LOW);
+  if(estadoDoBotaoDireito && auxiliar){
+      Serial.write("Seta Direita\n");
+    tempoAuxiliar = tempoAtual;
+    auxiliar = 0;
   }
-  
-  estadoA = digitalRead(BOTAOA); // Leitura para identificação do estado dos botões, se estão recebendo energia ou não
+    estadoA = digitalRead(BOTAOA); // Leitura para identificação do estado dos botões, se estão recebendo energia ou não
   estadoB = digitalRead(BOTAOB);
 
-   //As linhas de código abaixo são para quando os botões 
+  //As linhas de código abaixo são para quando os botões 
   //acionados, todos os leds conectados ao circuito acendam
-  // independentemente do botão ser o direito ou esquerdo, todos os leds ficarão acessos!
+ // independentemente do botão ser o direito ou esquerdo, todos os leds ficarão acessos!
    digitalWrite(LED, estadoA); 
    digitalWrite(LED, estadoB);
    digitalWrite(BLED, estadoA); 
